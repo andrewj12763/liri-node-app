fe876abe973eb35b3b20b0f20d78509977f554cc
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-04-07 10:09:52 -0600

    adding files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Xcn5tPLY9HJKyyADknBZTf7mI',
+  consumer_secret: '	jfGFfgDokgI9CAK7EJ1Q1cl2Y0rdseN9rPXPKOMqC6jTjWU89U',
+  access_token_key: '975899745666281472-Z17QInQVArTvDbNzetsxofIjVfUTyX5',
+  access_token_secret: 'yZMLJdFjDcE1iBCwHu3Y1RmPeHyuZBboNzKCCWhF0CRTS'
+}
