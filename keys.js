799c276221fd966ea2e47ca6d708e632624f2617
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-07-16 22:02:41 -0600

    fixing keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'Xcn5tPLY9HJKyyADknBZTf7mI',
-  consumer_secret: '	jfGFfgDokgI9CAK7EJ1Q1cl2Y0rdseN9rPXPKOMqC6jTjWU89U',
-  access_token_key: '975899745666281472-Z17QInQVArTvDbNzetsxofIjVfUTyX5',
-  access_token_secret: 'yZMLJdFjDcE1iBCwHu3Y1RmPeHyuZBboNzKCCWhF0CRTS'
+  consumer_key: 'AwjAXDZ28e8eGPvVHl6dHE51u',
+  consumer_secret: 'dbpifIyGR6viQ5cCDOyyKlPDDwoxmZvMbhH4LTuXLsrWvqieWb',
+  access_token_key: '3885326479-pYKYP7NfARIPh8fcseYNN8jsvjcW7a5kB0u7oiV',
+  access_token_secret: 'BimsVaruUP3k4JgvYcYN9xtqOVQDuQ1xKxKFfi3N2vf2t'
 }
